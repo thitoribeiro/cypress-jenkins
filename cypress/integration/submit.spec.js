fc8464d52da033a7d73cf7b05692a26cf1cc769c
thitoribeiro <thito.ribeiro@hotmail.com>
2022-02-02 17:47:41 -0300

    Initial commit

---
+++ cypress/integration/submit.spec.js
@@ -0,0 +1,11 @@
+const cypress = require("cypress")
+
+describe('Submit Form', () => {
+    it('Should fill and submit developer name', () => {
+        cy.visit("https://devexpress.github.io/testcafe/example/")
+        cy.get('#developer-name').type('Thito')
+        cy.get('#tried-test-cafe').click()
+        cy.get('#submit-button').click()
+        cy.get('#article-header').contains('Thank you')
+    })
+})
